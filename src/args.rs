4f7c79ae26fd9749372597fde148cac23915cf6d
Zack Pace <pacezipper@gmail.com>
2023-03-14 00:35:31 -0700

    feat: [cli] implement argument parsing using clap

---
+++ src/args.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+
+/// The command line arguments for Amp.
+#[derive(Parser)]
+#[command(name = "amp", bin_name = "amp", version)]
+pub enum Cli {
+    Build(Build),
+}
+
+/// The command line arguments for the `build` subcommand.
+#[derive(clap::Args)]
+#[command(author, version, about, long_about = None)]
+pub struct Build {
+    /// The path to the source file to compile.
+    pub input_path: String,
+
+    /// The path to output to.
+    #[clap(short)]
+    pub output_path: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::process::ExitCode;
 
+use args::Cli;
 use ast::Source;
+use clap::Parser;
 use codespan_reporting::{
     files::SimpleFiles,
     term::{self, termcolor::StandardStream},
 };
-use parser::Parser;
 use scanner::Scanner;
 use span::FileId;
 use typechecker::Typechecker;
 
 use crate::codegen::Codegen;
 
+pub mod args;
 pub mod ast;
 pub mod codegen;
 pub mod diagnostic;
@@ -22,15 +24,17 @@ pub mod span;
 pub mod typechecker;
 
 fn main() -> ExitCode {
+    let Cli::Build(args) = Cli::parse();
+
     let mut files = SimpleFiles::new();
 
     // Load the source file
     // TODO: read from command line arguments
-    let src = std::fs::read_to_string("test.amp").unwrap();
-    let file_id = FileId::new(files.add("test.amp", src));
+    let src = std::fs::read_to_string(&args.input_path).unwrap();
+    let file_id = FileId::new(files.add(args.input_path, src));
 
     let scanner = Scanner::new(file_id, files.get(file_id.0 as usize).unwrap().source());
-    let mut parser = Parser::new(scanner);
+    let mut parser = parser::Parser::new(scanner);
 
     let res = if let Some(res) = parser.parse::<Source>() {
         match res {
@@ -61,7 +65,7 @@ fn main() -> ExitCode {
 
     let mut codegen = Codegen::new();
     codegen.compile(checker);
-    std::fs::write("test.o", codegen.finish()).unwrap();
+    std::fs::write(args.output_path, codegen.finish()).unwrap();
 
     ExitCode::SUCCESS
 }
