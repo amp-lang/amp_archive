e08a71c82821801d0c869768c60baa640240b2b8
Zack Pace <pacezipper@gmail.com>
2023-03-16 21:37:19 -0700

    fix: accidentally committed everything

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn main() -> ExitCode {
     let scanner = Scanner::new(file_id, files.get(file_id.0 as usize).unwrap().source());
     let mut parser = parser::Parser::new(scanner);
 
-    let res = if let Some(res) = parser.parse::<Struct>() {
+    let res = if let Some(res) = parser.parse::<Source>() {
         match res {
             Ok(value) => value,
             Err(value) => {
@@ -53,34 +53,32 @@ fn main() -> ExitCode {
         return ExitCode::SUCCESS;
     };
 
-    dbg!(res);
+    let mut checker = Typechecker::new();
+    match checker.check(&res) {
+        Ok(_) => {}
+        Err(value) => {
+            diagnostic::display_diagnostic(&files, &value.as_diagnostic());
+            return ExitCode::FAILURE;
+        }
+    }
 
-    // let mut checker = Typechecker::new();
-    // match checker.check(&res) {
-    //     Ok(_) => {}
-    //     Err(value) => {
-    //         diagnostic::display_diagnostic(&files, &value.as_diagnostic());
-    //         return ExitCode::FAILURE;
-    //     }
-    // }
+    let mut codegen = Codegen::new();
+    codegen.compile(checker);
+    let binary = codegen.finish();
 
-    // let mut codegen = Codegen::new();
-    // codegen.compile(checker);
-    // let binary = codegen.finish();
-
-    // let file = NamedTempFile::new().unwrap();
-    // if let Err(_) = std::fs::write(&file, binary) {
-    //     diagnostic::display_diagnostic(
-    //         &files,
-    //         &Diagnostic::error().with_message("could not write object file"),
-    //     );
-    //     return ExitCode::FAILURE;
-    // }
-    // args.link.push(file.path().to_str().unwrap().to_owned());
-    // if let Err(_) = linker::link(&args.link, args.output_path) {
-    //     diagnostic::display_diagnostic(&files, &Diagnostic::error().with_message("linking failed"));
-    //     return ExitCode::FAILURE;
-    // }
+    let file = NamedTempFile::new().unwrap();
+    if let Err(_) = std::fs::write(&file, binary) {
+        diagnostic::display_diagnostic(
+            &files,
+            &Diagnostic::error().with_message("could not write object file"),
+        );
+        return ExitCode::FAILURE;
+    }
+    args.link.push(file.path().to_str().unwrap().to_owned());
+    if let Err(_) = linker::link(&args.link, args.output_path) {
+        diagnostic::display_diagnostic(&files, &Diagnostic::error().with_message("linking failed"));
+        return ExitCode::FAILURE;
+    }
 
     let compile_time = start_time.elapsed().as_nanos() as f64 / 1_000_000.0;
 
